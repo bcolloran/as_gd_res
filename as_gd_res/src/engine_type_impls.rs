4150d6d6e86626361c2cfb0053fc2c6a0180ee6f
brendan c <brendan@colloran.net>
2025-06-05 17:08:40 -0700

    update curve impl

--- as_gd_res/src/engine_type_impls.rs
+++ as_gd_res/src/engine_type_impls.rs
@@ -17,8 +17,38 @@ impl ExtractGd for Gd<PackedScene> {
     }
 }
 
+const CURVE_SAMPLE_POINTS: usize = 64;
+
 #[derive(Clone, Debug)]
-pub struct RustCurve(pub [f32; 100]);
+pub struct RustCurve {
+    baked: [f32; CURVE_SAMPLE_POINTS],
+    integral: f32,
+    max: f32,
+    min: f32,
+}
+
+impl RustCurve {
+    pub fn sample(&self, x: f32) -> f32 {
+        if x < self.min || x > self.max {
+            return 0.0;
+        }
+        let index = ((x - self.min) / (self.max - self.min) * (CURVE_SAMPLE_POINTS as f32 - 1.0))
+            .round() as usize;
+        self.baked[index]
+    }
+
+    pub fn integral(&self) -> f32 {
+        self.integral
+    }
+
+    pub fn max_domain(&self) -> f32 {
+        self.max
+    }
+
+    pub fn min_domain(&self) -> f32 {
+        self.min
+    }
+}
 
 impl_wrapped_as_gd_res!(RustCurve, Curve);
 
@@ -26,11 +56,20 @@ impl ExtractGd for Gd<Curve> {
     type Extracted = RustCurve;
 
     fn extract(&self) -> Self::Extracted {
-        let mut out = [0.0; 100];
-        for i in 0..100 {
-            out[i] = self.sample(i as f32 / 99.0);
+        let mut baked = [0.0; CURVE_SAMPLE_POINTS];
+        let mut integral = 0.0;
+        let dx = 1.0 / (CURVE_SAMPLE_POINTS as f32 - 1.0);
+        for i in 0..CURVE_SAMPLE_POINTS {
+            let y = self.sample(i as f32 / (CURVE_SAMPLE_POINTS as f32 - 1.0));
+            baked[i] = y;
+            integral += y * dx;
         }
-        RustCurve(out)
+        RustCurve {
+            baked,
+            integral,
+            max: self.get_max_domain(),
+            min: self.get_min_domain(),
+        }
     }
 }
 
