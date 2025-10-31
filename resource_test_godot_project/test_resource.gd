65d85abaeb0e190164453e2e9037911d8a5e2462
brendan c <brendan@colloran.net>
2025-10-30 18:49:52 -0700

    remove non-working GdUnit4

--- resource_test_godot_project/test_resource.gd
+++
@@ -1,9 +0,0 @@
-extends GdUnitTestSuite
-
-func test_complicated_resource() -> void:
-  assert_str("AbcD".to_lower()).is_equal("abcd")
-  #  load resource at res://test_complicated_resource.tres
-  var res: ComplicatedResource = load("res://test_complicated_resource.tres")
-  assert_object(res).is_not_null()
-  assert_int(res.value).is_equal(2)
-  assert_array(res.int_vec).is_equal([1, 2])
